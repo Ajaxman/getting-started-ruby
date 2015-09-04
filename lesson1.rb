ad60ce90b0d30cc9a28788021ce488fbb38c8f84
Javier Lopez Lopez <sjavierlopez@gmail.com>
2015-09-04 00:03:15 -0500

    The basic methods in ruby

---
+++ lesson1.rb
@@ -0,0 +1,7 @@
+puts "Hola bienvenido a ruby"
+puts "Cual es tu nombre?"
+name = gets.chomp
+puts "Cual es tu edad?"
+age = gets.chomp
+message = 'Bienvenido ' + name + ' tienes ' +  age + ' years old'
+puts message
