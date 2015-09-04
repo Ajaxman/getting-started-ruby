a146de899ac2c958974cde7d850af6f4137ed55e
Javier Lopez Lopez <sjavierlopez@gmail.com>
2015-09-04 09:44:45 -0500

    Adding more methods to array

---
+++ fourth-lesson.rb
@@ -0,0 +1,32 @@
+names = ['Ada', 'Belle', 'Chris']
+
+puts names
+
+languages = ['Spanish', 'German', 'English', 'Russian']
+
+puts "But also this printable using to_s method"
+puts languages.to_s
+
+
+languages.each do |item|
+  puts "Learning..." +  item
+end
+puts "End of program!"
+
+
+5.times do
+  puts  "Iteration.."
+end
+
+10.times do |iterator|
+
+  puts "The number is:" + iterator.to_s
+
+end
+
+puts "No working with multidimension arrays".center(400)
+puts "------->        <----------"
+
+web = [["jsp", "php", "ruby"], ["Angularjs", "Jquery"], ["R", "Erlang"]]
+
+puts web
