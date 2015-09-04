4ce7b2b71fd527232a2eef54cf3adaa1dbaa7210
Javier Lopez Lopez <sjavierlopez@gmail.com>
2015-09-04 00:22:04 -0500

    Second Lesson

---
+++ second-lesson.rb
@@ -0,0 +1,36 @@
+var1 = 'stop'
+var2 = 'underline'
+var3 = 'Can you pronunce this text in reverse'
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts var1
+puts var2
+puts var3
+
+puts "Whatis your name?"
+#name = gets.chomp
+name = 'javier'
+puts "your name is very small jajaja [size:" + name.length.to_s + "]"
+
+
+letters = 'aAbBcCdDeE'
+puts letters.upcase
+puts letters.downcase
+puts letters.swapcase
+puts letters.capitalize
+puts ' a'.capitalize
+puts letters
+
+lineWidth = 60
+
+puts (       'a'.center(lineWidth))
+puts       'v'.center(lineWidth)
+puts(          'When along came a spider'.center(lineWidth))
+puts(         'Which sat down beside her'.center(lineWidth))
+puts('And scared her poor shoe dog away.'.center(lineWidth))
+
+
+puts "1.- getting started.......".ljust(lineWidth/2) + ".....pag 1".rjust(lineWidth/2)
+puts "2.- Objects........".ljust(lineWidth/2) + "....pag 12".rjust(lineWidth/2)
